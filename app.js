39b4b108c0be85a249fd45b66b116440dd3707c4
Alexx <anapmsalvador@gmail.com>
2022-02-11 01:29:57 +0800

    added a few styles

--- app.js
+++ app.js
@@ -1,77 +1,101 @@
-choices = ["rock", "paper", "scissors"];
+
+
+
+// Document Query Selectors
+
 const rock = document.querySelector("#rock");
 const scissors = document.querySelector("#scissors");
 const paper = document.querySelector("#paper");
 const resultsPara = document.querySelector("#resultsPara");
 const restartButton = document.querySelector("#restartBtn");
-const restartPara = document.querySelector("#restartPara")
-
+const playerEmojiChoice = document.querySelector("#playerEmojiChoice");
+const computerEmojiChoice = document.querySelector("#computerEmojiChoice");
 const playerScoreDisplay = document.querySelector("#playerScoreDisplay");
 const computerScoreDisplay = document.querySelector("#computerScoreDisplay")
 const roundDisplay = document.querySelector("#roundDisplay")
 
+// Event Listeners 
+rock.addEventListener("click", () => playRound("✊", computerPlay()))
+paper.addEventListener("click", () => playRound("🖐", computerPlay()))
+scissors.addEventListener("click", () => playRound("✌️", computerPlay()))
+restartButton.addEventListener("click", () => restartGame())
 
-
+// Variables
 let playerScore = 0;
 let computerScore = 0;
 let numberOfRounds = 0;
+let choices = ["✊", "🖐", "✌️"];
+
+
+
+// PlayRound Function => starts game
 
 function playRound(playerChoice, computerChoice) {
     
-    playerSelection(playerChoice);
+
     computerPlay();
-    {
-        if( numberOfRounds < 5) {
+    restartButton.style.display = "block";
+
+        if( playerScore !== 3 || computerScore !== 3) {
+
             if (playerChoice === computerChoice) 
             {
-                    resultsPara.textContent = `Your choice: ${playerChoice}, Computer Choice: ${computerChoice}. It's a tie!`
+                resultsPara.textContent = `IT'S A TIE!`;
+                playerEmojiChoice.textContent = `😁 : ${playerChoice}`
+                computerEmojiChoice.textContent = `🤖 : ${computerChoice}`
             }   
 
-            else if( (playerChoice == "rock" && computerChoice == "paper") ||
-                (playerChoice == "paper" && computerChoice == "scissors") || 
-                (playerChoice == "scissors" && computerChoice == "rock") )
+            else if( (playerChoice == "✊" && computerChoice == "🖐") ||
+                (playerChoice == "🖐" && computerChoice == "✌️") || 
+                (playerChoice == "✌️" && computerChoice == "✊") )
             {
-                resultsPara.textContent = `Your choice: ${playerChoice}, Computer Choice: ${computerChoice}. YOU LOSE!`;
-                computerScore += 1;
-                currentComputerScore = computerScore;
-                computerScoreDisplay.innerText = currentComputerScore;
-                currentRound = numberOfRounds +=1
+                resultsPara.textContent = `YOU LOSE!`;
+                playerEmojiChoice.textContent = `😁 : ${playerChoice}`
+                computerEmojiChoice.textContent = `🤖 : ${computerChoice}`
+                computerScoreDisplay.innerText = computerScore += 1;
+                roundDisplay.innerHTML = numberOfRounds +=1;
+                checkWinner()
+               
 
             }
             else{
-                resultsPara.textContent = ` Your choice: ${playerChoice}, Computer Choice: ${computerChoice}. YOU WIN!`
-                playerScore += 1;
-                currentPlayerScore = playerScore;
-                playerScoreDisplay.innerText = currentPlayerScore;
-                numberOfRounds +=1;
-            
+                resultsPara.textContent = `YOU WIN!`;
+                playerEmojiChoice.textContent = `😁 : ${playerChoice}`
+                computerEmojiChoice.textContent = `🤖 : ${computerChoice}`
+                playerScoreDisplay.innerText = playerScore += 1;
+                roundDisplay.innerHTML = numberOfRounds +=1;
+                checkWinner()
             }
-        }
-        else  {
-            restartPara.textContent ="Game over! Press the button the restart the game!"
-        }
-        
+        } 
     }
-}
 
 
-rock.addEventListener("click", () => playRound("rock", computerPlay()))
-paper.addEventListener("click", () => playRound("paper", computerPlay()))
-scissors.addEventListener("click", () => playRound("scissors", computerPlay()))
-restartButton.addEventListener("click", () => restartRound())
 
 
-function playerSelection(playerSelected){
-    playerChoice = playerSelected.toLowerCase();;
-    return playerChoice ;
-}
-
-
+// Function Computer Play => Randomizes computer pick 
 function computerPlay(){
-    return  (choices[choice = Math.floor(Math.random() * 2)  + 1] ); 
+    return  (choices[Math.floor(Math.random() * 2)  + 1]); 
 }
 
-function restartRound() {
+// Function Restart Game => resets the game
+function restartGame() {
     window.location.reload(true);
 
+}
+
+// Function Check Winner => checks if game is over and there is already a winner 
+function checkWinner() {
+    if(playerScore == 3 || computerScore == 3) {
+
+        rock.disabled = true;
+        scissors.disabled = true;
+        paper.disabled = true;
+    
+        if(playerScore == 3){
+            resultsPara.textContent = "Good job, you won! Click the restart button to play again"
+        }
+        else {
+            resultsPara.textContent = "You lost the game, click the restart button to play again"
+        }
+    }
 }
