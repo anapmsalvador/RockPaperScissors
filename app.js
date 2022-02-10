2b9f952140115a34cba5494dd36858597a9911e2
Alexx <anapmsalvador@gmail.com>
2022-02-10 21:07:56 +0800

    added restart feature

--- app.js
+++ app.js
@@ -1,40 +1,64 @@
-
 choices = ["rock", "paper", "scissors"];
-
-
-function playRound(playerChoice, computerChoice) {
-
-        playerSelection(playerChoice);
-        computerPlay();
-
-        if (playerChoice === computerChoice) 
-        {
-                results.innerText = `Your choice: ${playerChoice}, Computer Choice: ${computerChoice}. It's a tie!`
-        }   
-
-        else if( (playerChoice == "rock" && computerChoice == "paper") ||
-            (playerChoice == "paper" && computerChoice == "scissors") || 
-            (playerChoice == "scissors" && computerChoice == "rock") )
-        {
-            results.innerText = `Your choice: ${playerChoice}, Computer Choice: ${computerChoice}. YOU LOSE!`
-        }
-        else{
-            results.innerText = ` Your choice: ${playerChoice}, Computer Choice: ${computerChoice}. YOU WIN!`
-        }
-        
-}
-
-
-
 const rock = document.querySelector("#rock");
 const scissors = document.querySelector("#scissors");
 const paper = document.querySelector("#paper");
-const results = document.querySelector("p")
+const resultsPara = document.querySelector("#resultsPara");
+const restartButton = document.querySelector("#restartBtn");
+const restartPara = document.querySelector("#restartPara")
+
+const playerScoreDisplay = document.querySelector("#playerScoreDisplay");
+const computerScoreDisplay = document.querySelector("#computerScoreDisplay")
+const roundDisplay = document.querySelector("#roundDisplay")
+
+
+
+let playerScore = 0;
+let computerScore = 0;
+let numberOfRounds = 0;
+
+function playRound(playerChoice, computerChoice) {
+    
+    playerSelection(playerChoice);
+    computerPlay();
+    {
+        if( numberOfRounds < 5) {
+            if (playerChoice === computerChoice) 
+            {
+                    resultsPara.textContent = `Your choice: ${playerChoice}, Computer Choice: ${computerChoice}. It's a tie!`
+            }   
+
+            else if( (playerChoice == "rock" && computerChoice == "paper") ||
+                (playerChoice == "paper" && computerChoice == "scissors") || 
+                (playerChoice == "scissors" && computerChoice == "rock") )
+            {
+                resultsPara.textContent = `Your choice: ${playerChoice}, Computer Choice: ${computerChoice}. YOU LOSE!`;
+                computerScore += 1;
+                currentComputerScore = computerScore;
+                computerScoreDisplay.innerText = currentComputerScore;
+                currentRound = numberOfRounds +=1
+
+            }
+            else{
+                resultsPara.textContent = ` Your choice: ${playerChoice}, Computer Choice: ${computerChoice}. YOU WIN!`
+                playerScore += 1;
+                currentPlayerScore = playerScore;
+                playerScoreDisplay.innerText = currentPlayerScore;
+                numberOfRounds +=1;
+            
+            }
+        }
+        else  {
+            restartPara.textContent ="Game over! Press the button the restart the game!"
+        }
+        
+    }
+}
 
 
 rock.addEventListener("click", () => playRound("rock", computerPlay()))
 paper.addEventListener("click", () => playRound("paper", computerPlay()))
 scissors.addEventListener("click", () => playRound("scissors", computerPlay()))
+restartButton.addEventListener("click", () => restartRound())
 
 
 function playerSelection(playerSelected){
@@ -44,9 +68,10 @@ function playerSelection(playerSelected){
 
 
 function computerPlay(){
-    computerChoice = (choices[choice = Math.floor(Math.random() * 2)  + 1] ); 
-    return computerChoice;
+    return  (choices[choice = Math.floor(Math.random() * 2)  + 1] ); 
 }
 
+function restartRound() {
+    window.location.reload(true);
 
-
+}
